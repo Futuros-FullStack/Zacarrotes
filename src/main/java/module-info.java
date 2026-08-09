6748fb8abfcf0ba2924a65066499792fbf239062
Alevalmor <alejandra.valdes95unach.mx>
2026-08-09 06:53:47 -0600

    Avance en módulo de productos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module michelle.zacarrotes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.postgresql.jdbc;
 
     opens michelle.zacarrotes to javafx.fxml;
     exports michelle.zacarrotes;
+
+    opens com.mycompany.zacarrotes to javafx.fxml;
+    exports com.mycompany.zacarrotes;
 }
